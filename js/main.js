2a9908aa52f49fbdce74b8c285c67490be5f32c1
NIKITA <nikitavegas95@gmail.com>
2022-10-26 00:48:31 +0300

    header - ok

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ let header = document.querySelector('header'),
     burgerBtn = document.querySelector('.burger-btn'),
     headerList = document.querySelector('.header-list'),
     headerNavPosition = document.querySelector('.header-nav_position'),
-    headerNav = document.querySelector('.header-nav');
+    headerNav = document.querySelector('.header-nav')
+    btn320 = document.querySelector('.btn-320')
+    headerAudio = document.querySelector('.header-audio');
 
 burgerBtn.addEventListener('click', function () {
   header.classList.toggle('header-open');
@@ -11,3 +13,7 @@ burgerBtn.addEventListener('click', function () {
   headerNavPosition.classList.toggle('header-nav_open');
   headerNav.classList.toggle('header-nav_open');
 });
+
+btn320.addEventListener('click', function () {
+  headerAudio.classList.toggle('header-audio_open');
+})
