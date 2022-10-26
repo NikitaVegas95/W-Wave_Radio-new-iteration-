85c277273704316c9dd99a81715ee5713aef1d33
NIKITA <nikitavegas95@gmail.com>
2022-10-26 19:15:19 +0300

    header - popUp 75%

--- js/main.js
+++ js/main.js
@@ -4,9 +4,12 @@ let header = document.querySelector('header'),
     headerNavPosition = document.querySelector('.header-nav_position'),
     headerNav = document.querySelector('.header-nav')
     btn320 = document.querySelector('.btn-320')
-    headerAudio = document.querySelector('.header-audio');
+    headerAudio = document.querySelector('.header-audio')
+    headerBtn = document.querySelector('.header-btn')
+    headerModal = document.querySelector('.header__modal')
+    headerPopupClose = document.querySelector('.header__popup-close');
 
-burgerBtn.addEventListener('click', function () {
+burgerBtn.addEventListener('click', () => {
   header.classList.toggle('header-open');
   burgerBtn.classList.toggle('burger-btn_close');
   headerList.classList.toggle('header-list_after');
@@ -14,6 +17,20 @@ burgerBtn.addEventListener('click', function () {
   headerNav.classList.toggle('header-nav_open');
 });
 
-btn320.addEventListener('click', function () {
+btn320.addEventListener('click', () => {
   headerAudio.classList.toggle('header-audio_open');
-})
+});
+
+headerBtn.addEventListener('click', () => {
+    headerModal.classList.add('active');
+});
+
+headerPopupClose.addEventListener('click', () => {
+    headerModal.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target === headerModal) {
+        headerModal.classList.remove('active');
+    }
+});
