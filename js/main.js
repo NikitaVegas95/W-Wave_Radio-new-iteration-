b3b556a0b60bb8481227b99da1883388b2046733
NIKITA <nikitavegas95@gmail.com>
2022-10-30 06:51:43 +0300

    header - ok hero - ok

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ let header = document.querySelector('header'),
     burgerBtn = document.querySelector('.burger-btn'),
     headerList = document.querySelector('.header-list'),
     headerNavPosition = document.querySelector('.header-nav_position'),
-    headerNav = document.querySelector('.header-nav')
-    btn320 = document.querySelector('.btn-320')
-    headerAudio = document.querySelector('.header-audio')
-    headerBtn = document.querySelector('.header-btn')
-    headerModal = document.querySelector('.header__modal')
-    headerPopupClose = document.querySelector('.header__popup-close');
+    headerNav = document.querySelector('.header-nav'),
+    btn320 = document.querySelector('.btn-320'),
+    headerAudio = document.querySelector('.header-audio'),
+    headerBtn = document.querySelector('.header-btn'),
+    headerModal = document.querySelector('.header__modal'),
+    headerPopupClose = document.querySelector('.header__popup-close'),
+    headerBtnPast = document.querySelector('.header-btn-past'),
+    displayActive = document.querySelector('.display-active'),
+    displayNone = document.querySelector('.display-none'),
+    headerBtnLive = document.querySelector('.header-btn-live'),
+    displayActiveNaw = document.querySelector('.display-active-naw'),
+    displayNoneNaw = document.querySelector('.display-none-naw');
+
+
+
 
 burgerBtn.addEventListener('click', () => {
   header.classList.toggle('header-open');
@@ -33,4 +42,14 @@ document.addEventListener('click', (e) => {
     if (e.target === headerModal) {
         headerModal.classList.remove('active');
     }
+});
+
+headerBtnPast.addEventListener('click', () => {
+    displayActive.classList.toggle('display-none');
+    displayNone.classList.toggle('display-block');
+});
+
+headerBtnLive.addEventListener('click', () => {
+    displayActiveNaw.classList.toggle('display-none-naw');
+    displayNoneNaw.classList.toggle('display-block-naw');
 });
